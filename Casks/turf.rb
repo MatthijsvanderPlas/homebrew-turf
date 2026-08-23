2d99681a245f9d444b770ac4ac0fffa2a964f5b3
turf-release <actions@github.com>
2026-08-23 10:59:14 +0000

    Bump turf to 1.5.0

--- Casks/turf.rb
+++ Casks/turf.rb
@@ -1,6 +1,6 @@
 cask "turf" do
-  version "1.4.0"
-  sha256 "b7bc5bc18b6d4d5f621df69a945994492946409eef94ec3c02d4f318d7eec22e"
+  version "1.5.0"
+  sha256 "e2ce21c429e1f11964ad4add259dd7817480258fa02f4d74877c52d83dd6a08a"
 
   url "https://github.com/MatthijsvanderPlas/turf-releases/releases/download/v#{version}/Turf-#{version}.zip"
   name "Turf"
